3999cbfe824ac8889d2364264e691ff76982690a
Karina Gurachevskaya <k.gurachevskaya@gmail.com>
2022-11-22 14:56:10 +0100

    Project init

---
+++ CatsNetworkWidget/CatsNetworkWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsNetworkWidgetApp.swift
+//  CatsNetworkWidget
+//
+//  Created by Karina gurachevskaya on 22.11.22.
+//
+
+import SwiftUI
+
+@main
+struct CatsNetworkWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
